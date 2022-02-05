33490abadd535c7d3cae6eba62f78a69baafc903
Bob Monsour <bob.monsour@gmail.com>
2022-02-05 15:43:49 -0800

    Added sitemap

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: "development",
   title: "Bob Monsour | Home",
-  baseurl: "http://localhost:8080",
+  url: "http://localhost:8080",
   mainNavLinks: [{ url: "/archives/", text: "Archives", class: "nav__item" }],
 };
