fb0740e21de618b6e48f8e7f4115de69a253b43f
Bob Monsour <bob.monsour@gmail.com>
2022-07-18 18:49:20 -0700

    Postcss with open props, cssnano, autoprefixer, and postcss-logical

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 // postcss.config.js
 const postcssJitProps = require("postcss-jit-props");
 const OpenProps = require("open-props");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const postcssLogical = require("postcss-logical");
 
 module.exports = {
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [postcssJitProps(OpenProps), postcssLogical, cssnano, autoprefixer],
 };
