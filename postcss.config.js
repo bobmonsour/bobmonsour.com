491a3b90b5161d1e71148c95f86c43720040c3f8
Bob Monsour <bob.monsour@gmail.com>
2023-01-01 07:27:59 -0800

    Blockquote border test 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 // postcss.config.js
 const cssnano = require("cssnano");
 const autoprefixer = require("autoprefixer");
-const postcssLogical = require("postcss-logical");
+const postcssLogical = require("postcss-logical")({
+  dir: "ltr",
+});
 
 module.exports = {
   plugins: [postcssLogical, cssnano, autoprefixer],
