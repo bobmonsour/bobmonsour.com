c7ec315478353c9d944ed9aa2724cd9f79b7ad7d
Bob Monsour <bob.monsour@gmail.com>
2023-01-22 12:16:42 -0800

    New post about piano learning, re-posting 2 deleted posts, and fixing my sitemap

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
   dev_url: "http://localhost:8080",
   prod_url: "https://bobmonsour.com",
   title: "Bob Monsour | Home",
-  url: "http://www.bobmonsour.com",
+  url: "https://www.bobmonsour.com",
   mainNavLinks: [
     { url: "/archives/", text: "Archives", class: "nav__item" },
     { url: "/about/", text: "About", class: "nav__item" },
