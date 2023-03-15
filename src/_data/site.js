a9d37f765cf53ed134ecfb1d9c85b3a7d8a835b7
Bob Monsour <bob.monsour@gmail.com>
2023-03-15 09:03:15 -0700

    Hopefully a final correction to the RSS feed

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,9 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
-  production_url: "https://bobmonsour.com",
   title: "Bob Monsour | Home",
   description:
     "The personal website of Bob Monsour. Enjoying learning and deploying websites with 11ty and Netlify.",
-  url: "https://www.bobmonsour.com",
+  url: "https://www.bobmonsour.com/",
   language: "en",
   author: {
     name: "Bob Monsour",
