70c5ebe7daab6de523c32280fa40163864bf6ea2
Bob Monsour <bob.monsour@gmail.com>
2024-12-23 21:29:13 -0800

    Added currently reading and undated section

--- src/_data/books.js
+++ src/_data/books.js
@@ -5,15 +5,42 @@ const require = createRequire(import.meta.url);
 const theBooks = require("./books.json");
 
 export default async function () {
-	// exclude books that have 'undated' as the value for the yearRead property
-	const filteredBooks = theBooks.filter((book) => book.yearRead != "undated");
-	const undatedBooks = theBooks.filter((book) => book.yearRead === "undated");
+	// generate an object representing the book currently being read
+	const currentBook = theBooks.filter((book) => book.yearRead === "currently");
+
+	// generate an array of books with dates, excluding the book currently being read
+	const datedBooks = theBooks
+		.filter(
+			(book) => book.yearRead != "undated" && book.yearRead != "currently"
+		)
+		.sort((a, b) => {
+			return a.yearRead > b.yearRead ? -1 : 1;
+		});
+
+	// generate an array of books with yearRead as 'undated'
+	const undatedBooks = theBooks.filter(
+		(book) => book.yearRead === "undated" && book.yearRead != "currently"
+	);
 	// generate the sorted books
-	const datedBooks = filteredBooks.sort((a, b) => {
-		return a.yearRead > b.yearRead ? -1 : 1;
-	});
+	// const datedBooks = filteredBooks.sort((a, b) => {
+	// 	return a.yearRead > b.yearRead ? -1 : 1;
+	// });
+
+	// Extract unique years and format them
+	const uniqueYears = [
+		...new Set(datedBooks.map((book) => book.yearRead.split("/")[0])),
+	]
+		.map((year) => `y${year}`)
+		.sort((a, b) => b.slice(1) - a.slice(1)); // Sort in descending order
+
+	// Generate the result array
+	const years = uniqueYears.map((year) => ({ year }));
+	years.push({ year: "undated" });
+
 	return {
+		currentBook,
 		datedBooks,
 		undatedBooks,
+		years,
 	};
 }
