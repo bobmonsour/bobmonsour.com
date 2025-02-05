d22552983912aaeaadcb04ac93beaf3f0725b278
Bob Monsour <bob.monsour@gmail.com>
2025-02-05 15:39:17 -0800

    More comment editing on books.js for 11ty meetup

--- src/_data/books.js
+++ src/_data/books.js
@@ -20,6 +20,8 @@ export default async function () {
 	const undatedBooks = theBooks.filter((book) => book.yearRead === "undated");
 
 	// Extract unique years and format them, sorted in descending order
+	// result array looks like this:
+	//   ["y2025", "y2024", etc.]
 	const uniqueYears = [
 		...new Set(datedBooks.map((book) => book.yearRead.split("/")[0])),
 	]
@@ -27,6 +29,8 @@ export default async function () {
 		.sort((a, b) => b.slice(1) - a.slice(1));
 
 	// Generate the years array for creating the year nav
+	// resulting array looks like this:
+	//   [ { year: "y2025" }, { year: "y2024" },..., { year: "undated" } ]
 	const years = uniqueYears.map((year) => ({ year }));
 	years.push({ year: "undated" });
 
